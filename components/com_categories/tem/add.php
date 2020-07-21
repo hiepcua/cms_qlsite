1a077c2cab204c6ce5337fc9f28f44f1c2a2ad01
Tran Hiep <tranviethiepdz@gmail.com>
2020-07-21 09:41:36 +0700

    update com category

--- components/com_categories/tem/add.php
+++ components/com_categories/tem/add.php
@@ -45,6 +45,30 @@ if(isset($_POST['cmdsave_tab1']) && $_POST['txt_name']!='') {
 		$_SESSION['flash'.'com_'.COMS] = 0;
 	}
 }
+
+function getListComboboxSites($parid=0, $level=0, $childs=array()){
+	$sql="SELECT * FROM tbl_sites WHERE `par_id`='$parid' AND `isactive`='1' ";
+	$objdata=new CLS_MYSQL();
+	$objdata->Query($sql);
+	$char="";
+	if($level!=0){
+		for($i=0;$i<$level;$i++)
+			$char.="|-----";
+	}
+	if($objdata->Num_rows()<=0) return;
+	while($rows=$objdata->Fetch_Assoc()){
+		$id=$rows['id'];
+		$parid=$rows['par_id'];
+		$title=$rows['title'];
+		if(in_array($id, $childs)){
+			echo "<option value='$id' disabled='true' class='disabled'>$char $title</option>";
+		}else{
+			echo "<option value='$id'>$char $title</option>";
+		}
+		$nextlevel=$level+1;
+		getListComboboxSites($id,$nextlevel, $childs);
+	}
+}
 ?>
 <!-- Content Header (Page header) -->
 <div class="content-header">
